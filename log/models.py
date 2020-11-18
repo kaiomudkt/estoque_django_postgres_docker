8484f682124167b8419907bdcc91fd4db8d451cf
rafael-junio <rafael-954@hotmail.com>
2020-11-17 20:27:21 -0400

    Trigger corrigida

--- log/models.py
+++ log/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 
 class Log(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_tabela = 1
-    id_tupla = 1
-    metodo = 'atualizar'
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_tabela = models.CharField(max_length=50, null=True)
+    id_tupla = models.CharField(max_length=50, null=True)
+    metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
-    atualizado_em = models.DateTimeField(auto_now=True)
--- trigger.sql
+++ trigger.sql
@@ -1,29 +1,23 @@
 CREATE OR REPLACE FUNCTION log() RETURNS TRIGGER AS $$
-	BEGIN 
-		IF (TG_RELNAME = 'catalogo') then
-			IF(TG_OP = 'UPDATE') then
-				INSERT INTO log (id_tabela, id_tupla, metodo, data) 
-					values (TG_RELNAME, OLD.id, 'UPDATE', CURRENT_TIMESTAMP);
-			END IF;
-			IF(TG_OP = 'DELETE') then
-				INSERT INTO log (id_tabela, id_tupla, metodo, data) 
-					values (TG_RELNAME, OLD.id, 'DELETE', CURRENT_TIMESTAMP);
-			END IF;
-			IF(TG_OP = 'INSERT') then
-				INSERT INTO log (id_tabela, id_tupla, metodo, data) 
-					values (TG_RELNAME, NEW.id, 'DELETE', CURRENT_TIMESTAMP);
-			END IF;
-
-		ELSIF (TG_RELNAME = 'fornecedor') then
-			IF(TG_OP = UPDATE) then
-				INSERT INTO log () values ();
-			END IF;
-		END IF;
-
+	BEGIN
+        IF (TG_OP = 'UPDATE') then
+            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
+                values (TG_RELNAME, OLD.id, 'UPDATE', CURRENT_TIMESTAMP);
+            RETURN OLD;
+        ELSIF (TG_OP = 'DELETE') then
+            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
+                values (TG_RELNAME, OLD.id, 'DELETE', CURRENT_TIMESTAMP);
+            RETURN OLD;
+        ELSIF (TG_OP = 'INSERT') then
+            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
+                values (TG_RELNAME, NEW.id, 'INSERT', CURRENT_TIMESTAMP);
+            RETURN NEW;
+        END IF;
 	END;
+
 $$ LANGUAGE PLPGSQL;
 
 
 CREATE TRIGGER log
-BEFORE UPDATE or INSERT or DELETE ON produto or fornecedor
+BEFORE UPDATE or INSERT or DELETE ON catalogo_catalogo
 FOR EACH ROW EXECUTE PROCEDURE log();

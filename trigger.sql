0f8eba84e8b3aaf9e83386e48e60c1b3ac2c14e7
kaiomudkt <kaiomudkt@gmail.com>
2020-11-18 22:44:56 -0400

    criando models dos logs

--- trigger.sql
+++ trigger.sql
@@ -37,3 +37,20 @@ $$ LANGUAGE PLPGSQL;
 CREATE TRIGGER log
 BEFORE UPDATE or INSERT or DELETE ON fornecedor_fornecedor
 FOR EACH ROW EXECUTE PROCEDURE logFornecedor();
+
+-- CATALOGO
+CREATE OR REPLACE FUNCTION logFornecedor() RETURNS TRIGGER AS $$
+	BEGIN
+        INSERT INTO log_log (metodo, data, a_id, , a_nome, a_cnpj, a_catalogo, n_id, n_nome, n_cnpj, n_catalogo)
+            values (TG_OP, CURRENT_TIMESTAMP, OLD.id, OLD.nome, OLD.cnpj, OLD.catalogo, n_id, NEW.nome, NEW.cnpj, NEW.catalogo);
+        IF (TG_OP = 'DELETE') then
+            RETURN OLD;
+        ELSE
+            RETURN NEW;
+        END IF;
+	END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER log
+BEFORE UPDATE or INSERT or DELETE ON fornecedor_fornecedor
+FOR EACH ROW EXECUTE PROCEDURE logFornecedor();

31eacce65bd55850fa5be116178668c253a5d4c7
kaiomudkt <kaiomudkt@gmail.com>
2020-11-18 23:25:53 -0400

    arrumando as triggers, ainda falta testar

--- log/models.py
+++ log/models.py
@@ -1,9 +1,9 @@
 # from django.contrib.auth.models import User
 from django.db import models
-
+# a de antigo, n de novo
 class LogCatalogo(models.Model):
-    id_tabela = models.CharField(max_length=50, null=True)
-    id_tupla = models.CharField(max_length=50, null=True)
+    a_id_tupla = models.CharField(max_length=50, null=True)
+    n_id_tupla = models.CharField(max_length=50, null=True)
     metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
     a_nome = models.CharField(max_length=255)
@@ -14,7 +14,8 @@ class LogCatalogo(models.Model):
     n_id_preco = models.CharField(max_length=50, null=True)
 
 class LogPreco(models.Model):
-    id_tupla = models.CharField(max_length=50, null=True)
+    a_id_tupla = models.CharField(max_length=50, null=True)
+    n_id_tupla = models.CharField(max_length=50, null=True)
     metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
     a_dolar = models.DecimalField(max_digits=9, decimal_places=2)
@@ -23,7 +24,8 @@ class LogPreco(models.Model):
     n_real = models.DecimalField(max_digits=9, decimal_places=2)
 
 class LogFornecedor(models.Model):
-    id_tupla = models.CharField(max_length=50, null=True)
+    a_id_tupla = models.CharField(max_length=50, null=True)
+    n_id_tupla = models.CharField(max_length=50, null=True)
     metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
     a_nome = models.CharField(max_length=255)
@@ -34,7 +36,8 @@ class LogFornecedor(models.Model):
     n_id_catalogo = models.CharField(max_length=50, null=True)
 
 class LogLote(models.Model):
-    id_tupla = models.CharField(max_length=50, null=True)
+    a_id_tupla = models.CharField(max_length=50, null=True)
+    n_id_tupla = models.CharField(max_length=50, null=True)
     metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
     a_data_lote = models.DateTimeField(auto_now_add=True)
@@ -45,7 +48,8 @@ class LogLote(models.Model):
     n_id_produtoFornecedor = models.CharField(max_length=50, null=True)
 
 class LogProdutoFornecedor(models.Model):
-    id_tupla = models.CharField(max_length=50, null=True)
+    a_id_tupla = models.CharField(max_length=50, null=True)
+    n_id_tupla = models.CharField(max_length=50, null=True)
     metodo = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True)
     a_id_fornecedor = models.CharField(max_length=50, null=True)
--- trigger.sql
+++ trigger.sql
@@ -1,31 +1,29 @@
 -- CATALOGO
-CREATE OR REPLACE FUNCTION log() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION logCatalogo() RETURNS TRIGGER AS $$
 	BEGIN
-        IF (TG_OP = 'UPDATE') then
-            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
-                values (TG_RELNAME, OLD.id, 'UPDATE', CURRENT_TIMESTAMP);
+        INSERT INTO log_log (a_id_tupla, n_id_tupla, metodo, data, a_nome, n_nome, a_descricao, n_descricao,
+                a_id_preco, n_id_preco)
+            values (OLD.id, NEW.id, TG_OP, CURRENT_TIMESTAMP, OLD.nome, new.nome, OLD.descricao, NEW.descricao,
+                OLD.preco_id, NEW.preco_id);
+        IF (TG_OP = 'DELETE') then
             RETURN OLD;
-        ELSIF (TG_OP = 'DELETE') then
-            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
-                values (TG_RELNAME, OLD.id, 'DELETE', CURRENT_TIMESTAMP);
-            RETURN OLD;
-        ELSIF (TG_OP = 'INSERT') then
-            INSERT INTO log_log (id_tabela, id_tupla, metodo, data)
-                values (TG_RELNAME, NEW.id, 'INSERT', CURRENT_TIMESTAMP);
+        ELSE
             RETURN NEW;
         END IF;
 	END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER log
+CREATE TRIGGER log_catalogo
 BEFORE UPDATE or INSERT or DELETE ON catalogo_catalogo
-FOR EACH ROW EXECUTE PROCEDURE log();
+FOR EACH ROW EXECUTE PROCEDURE logCatalogo();
 
 -- FORNECEDOR
 CREATE OR REPLACE FUNCTION logFornecedor() RETURNS TRIGGER AS $$
 	BEGIN
-        INSERT INTO log_log (metodo, data, a_id, , a_nome, a_cnpj, a_catalogo, n_id, n_nome, n_cnpj, n_catalogo)
-            values (TG_OP, CURRENT_TIMESTAMP, OLD.id, OLD.nome, OLD.cnpj, OLD.catalogo, n_id, NEW.nome, NEW.cnpj, NEW.catalogo);
+        INSERT INTO log_log (a_id_tupla, n_id_tupla, metodo, data, a_nome, n_nome, a_cnpj, n_cnpj,
+                a_id_catalogo, n_id_catalogo)
+            values (OLD.id, NEW.id, TG_OP, CURRENT_TIMESTAMP, OLD.nome, new.nome, OLD.cnpj, NEW.cnpj,
+                OLD.id_catalogo, NEW.id_catalogo);
         IF (TG_OP = 'DELETE') then
             RETURN OLD;
         ELSE
@@ -34,23 +32,7 @@ CREATE OR REPLACE FUNCTION logFornecedor() RETURNS TRIGGER AS $$
 	END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER log
-BEFORE UPDATE or INSERT or DELETE ON fornecedor_fornecedor
+CREATE TRIGGER log_fornecedor
+BEFORE UPDATE or INSERT or DELETE ON catalogo_fornecedor
 FOR EACH ROW EXECUTE PROCEDURE logFornecedor();
 
--- CATALOGO
-CREATE OR REPLACE FUNCTION logFornecedor() RETURNS TRIGGER AS $$
-	BEGIN
-        INSERT INTO log_log (metodo, data, a_id, , a_nome, a_cnpj, a_catalogo, n_id, n_nome, n_cnpj, n_catalogo)
-            values (TG_OP, CURRENT_TIMESTAMP, OLD.id, OLD.nome, OLD.cnpj, OLD.catalogo, n_id, NEW.nome, NEW.cnpj, NEW.catalogo);
-        IF (TG_OP = 'DELETE') then
-            RETURN OLD;
-        ELSE
-            RETURN NEW;
-        END IF;
-	END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE TRIGGER log
-BEFORE UPDATE or INSERT or DELETE ON fornecedor_fornecedor
-FOR EACH ROW EXECUTE PROCEDURE logFornecedor();
